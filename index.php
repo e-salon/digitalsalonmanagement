95f94044722c7f727ddf88762ce356f5006c3d31
Kaiza Ilomo <kunonukaiza@gmail.com>
2020-07-12 00:38:35 +0300

    add file
    added project files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My digital salon</title>
+        <link rel="stylesheet" href="pages/.css/styles.css" >
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo '<h1> Welcome to our system<br></h1>';    
+         ?>
+        <h2>Customer?-<a href="pages/.html/customer registration form.html">Click here to book a service at your favorite salon</a> </h2>
+        <br>
+        <h2>Salon owner?-<a href="pages/.html/salon manager registration form.html">Click here to manage your bookings</a></h2>
+    </body>
+</html>
---
+++ pages/.php/customerData.php
@@ -0,0 +1,56 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = customerData;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully<br>";
+
+$sql = "INSERT INTO MtejaData (firstName, lastName, mobileNumber, email, age, password )
+VALUES ('".$_POST['firstName']."', '".$_POST['lastName']."', '".$_POST['mobileNumber']."','".$_POST['email']."',"
+        . "'".$_POST['age']."', '".$_POST['password']."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+//$say = htmlspecialchars($_POST['firstName']);
+//echo $say;
+
+// sql to create table
+/*$sql = "CREATE TABLE MtejaData(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+mobileNumber VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+age VARCHAR(50),
+password VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}*/
+
+$conn->close();
+?>
