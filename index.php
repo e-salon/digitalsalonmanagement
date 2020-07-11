5256c12d019079b435288e52fd36bf6a9dbec54c
Kaiza Ilomo <kunonukaiza@gmail.com>
2020-07-12 01:13:23 +0300

    edit homepage wordind
    edit project homepage interface
    
    Co-Authored-By: bugongo <67264266+bugongo@users.noreply.github.com>
    Co-Authored-By: naomi03 <67288767+naomi03@users.noreply.github.com>
    Co-Authored-By: annmeth <67288898+annmeth@users.noreply.github.com>
    Co-Authored-By: yusuphmasha <67264348+yusuphmasha@users.noreply.github.com>

--- index.php
+++ index.php
@@ -7,14 +7,16 @@
         <link rel="stylesheet" href="pages/.css/styles.css" >
     </head>
     <body>
+    <center>
         <?php
         // put your code here
         echo '<h1> Welcome to our system<br></h1>';    
          ?>
-        <h2>Customer?-<a href="pages/.html/customer registration form.html">Click here to book a service at your favorite salon</a> </h2>
+        <h2>Are you customer?-<a href="pages/.html/customer registration form.html">Click here to book a service at your favorite salon</a> </h2>
         <br>
-        <h2>Salon owner?-<a href="pages/.html/salon manager registration form.html">Click here to manage your bookings</a></h2>
+        <h2>Are you a salon owner/manager?-<a href="pages/.html/salon manager registration form.html">Click here to manage your bookings</a></h2>
         
         <h4>Encountered a problem?<a href="#">Let us know.</a></h4>
+    </center>
     </body>
 </html>
