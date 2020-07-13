846b7731e3d381f052cb385325859e5c9feb1958
Kaiza Ilomo <kunonukaiza@gmail.com>
2020-07-13 21:14:47 +0300

    remove whitespaces in file names
     removed all whiltespaces in all file names to adhere to file naming rules
    
    Co-Authored-By: bugongo <67264266+bugongo@users.noreply.github.com>
    Co-Authored-By: annmeth <67288898+annmeth@users.noreply.github.com>
    Co-Authored-By: naomi03 <67288767+naomi03@users.noreply.github.com>
    Co-Authored-By: yusuphmasha <67264348+yusuphmasha@users.noreply.github.com>

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
     <body>
     <center>
         <?php
-        // put your code here
-        echo '<h1> Welcome to our system<br></h1>';    
+        
+        echo '<h1> Welcome to Nisuke system<br>'
+        . ' Arranging appointments with your favourite hairdresser has been made easier</h1>';    
          ?>
-        <h2>Are you customer?-<a href="pages/.html/customer registration form.html">Click here to book a service at your favorite salon</a> </h2>
+        <h2>Are you customer?-<a href="pages/.html/customerregistrationform.html">Click here to book a service at your favorite salon</a> </h2>
         <br>
-        <h2>Are you a salon owner/manager?-<a href="pages/.html/salon manager registration form.html">Click here to manage your bookings</a></h2>
+        <h2>Are you a salon owner/manager?-<a href="pages/.html/salonManageRegistrationForm.html">Click here to manage your bookings</a></h2>
         
         <h4>Encountered a problem?<a href="#">Let us know.</a></h4>
     </center>
