4891a733378bf34d2bf1d28bba6b303a43bd8598
Kaiza Ilomo <kunonukaiza@gmail.com>
2020-07-13 23:54:49 +0300

    center  form field boxes
    centered form fielsds
    
    Co-Authored-By: bugongo <67264266+bugongo@users.noreply.github.com>
    Co-Authored-By: annmeth <67288898+annmeth@users.noreply.github.com>
    Co-Authored-By: naomi03 <67288767+naomi03@users.noreply.github.com>
    Co-Authored-By: yusuphmasha <67264348+yusuphmasha@users.noreply.github.com>

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
         echo '<h1> Welcome to Nisuke system<br>'
         . ' Arranging appointments with your favourite hairdresser has been made easier</h1>';    
          ?>
-        <h2>Are you customer?-<a href="pages/.html/customerregistrationform.html">Click here to book a service at your favorite salon</a> </h2>
+        <h2>Are you customer?-<a href="pages/.html/customerRegistration.html">Click here to book a service at your favorite salon</a> </h2>
         <br>
-        <h2>Are you a salon owner/manager?-<a href="pages/.html/salonManageRegistrationForm.html">Click here to manage your bookings</a></h2>
+        <h2>Are you a salon owner/manager?-<a href="pages/.html/bossRegistration.html">Click here to manage your bookings</a></h2>
         
         <h4>Encountered a problem?<a href="#">Let us know.</a></h4>
     </center>
